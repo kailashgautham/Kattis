c2f765c4ee34e35a87513447313d8fd357342662
kailashgautham <kailash.gautham@gmail.com>
2023-06-29 16:16:13 +0700

    completed fifth question for the day

---
+++ numberfun.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    float a,b,c;
+    for(int i=0;i<n;i++){
+        cin>>a>>b>>c;
+        if(a+b==c||a-b==c||b-a==c||a*b==c||a/b==c||b/a==c)cout<<"Possible"<<endl;
+        else cout<<"Impossible"<<endl;
+    }
+}
